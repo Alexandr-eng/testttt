48c10d397228a9c93213cd2334dcca7feee9760e
Admin <kopylov.a98@mail.ru>
2025-01-24 06:36:14 +0500

    add test

--- shop/cart/tests.py
+++ shop/cart/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your tests here.
+class CartTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.product = Product.objects.create(name='Test Product', price=10.00)
+
+    def test_add_to_cart(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse('cart-list')
+        data = {'product': self.product.id, 'quantity': 2}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- shop/products/tests.py
+++ shop/products/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+class ProductTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_products(self):
+        url = reverse('product-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
